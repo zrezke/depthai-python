8db31c9b9985b61e87566eb0c31ed99696afdd86
alex-luxonis <alex@luxonis.com>
2021-01-07 06:07:42 +0200

    ColorCamera add bindings: `isp`/`raw` outputs, disable post-proc

--- src/pipeline/NodeBindings.cpp
+++ src/pipeline/NodeBindings.cpp
@@ -86,6 +86,8 @@ void NodeBindings::bind(pybind11::module& m){
         .def_readonly("video", &ColorCamera::video)
         .def_readonly("preview", &ColorCamera::preview)
         .def_readonly("still", &ColorCamera::still)
+        .def_readonly("isp", &ColorCamera::isp)
+        .def_readonly("raw", &ColorCamera::raw)
         .def("setCamId", [](ColorCamera& c, int64_t id) {
             // Issue an deprecation warning
             PyErr_WarnEx(PyExc_DeprecationWarning, "setCamId() is deprecated, use setBoardSocket() instead.", 1);
@@ -138,6 +140,7 @@ void NodeBindings::bind(pybind11::module& m){
         .def("getWaitForConfigInput", &ColorCamera::getWaitForConfigInput)
         .def("setPreviewKeepAspectRatio", &ColorCamera::setPreviewKeepAspectRatio)
         .def("getPreviewKeepAspectRatio", &ColorCamera::getPreviewKeepAspectRatio)
+        .def("setEnablePreviewStillVideoStreams", &ColorCamera::setEnablePreviewStillVideoStreams)
         ;
 
     // NeuralNetwork node
